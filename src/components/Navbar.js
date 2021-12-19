2ea5286339a9269e29d5811ae3573de317c55839
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2021-12-20 00:41:22 +0700

    add navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ import style from '../assets/style/navbar.module.css'
 
 const Navbar = () => {
   return (
-    <div className={`${style.navbar} navbar p-3 navbar-expand-lg navbar-light bg-dark`}>
+    <div className={`${style.navbar} navbar p-3 navbar-expand-md navbar-light bg-dark`}>
       {/* ---- Container Section ---- */}
       <div className='container d-flex justify-content-between'>
 
@@ -23,7 +23,7 @@ const Navbar = () => {
             <a className={`${style.a}`} href="/#"><li className="nav-item px-3">Check Invoice</li></a>
             <a className={`${style.a}`} href="/#"><li className="nav-item px-3">Features</li></a>
             <a className={`${style.a}`} href="/#"><li className="nav-item px-3">Help</li></a>
-            <a className={`${style.a}`} href="/#"><li className="nav-item ps-3">Login</li></a>
+            <a className={`${style.a}`} href="/login"><li className="nav-item ps-3">Login</li></a>
           </ul>
       </div>
     </div>
