1754b02e30308b9e553afc1823466438794703e7
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2021-12-22 14:49:45 +0700

    add login service

---
+++ src/services/login.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const baseUrl = 'https://gen-voice-be.herokuapp.com/login'
+
+const login = async data => {
+  const response = await axios.post(baseUrl, data)
+  return response.data
+}
+
+export default login
