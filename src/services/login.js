4882686ed4ed4fc45b38b9ae42b9d32424d357dc
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2021-12-22 16:34:24 +0700

    change url

--- src/services/login.js
+++ src/services/login.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const baseUrl = 'https://gen-voice-be.herokuapp.com/users/login'
 
 const login = async data => {
-  const response = await axios.post(baseUrl, data)
+  const response = await axios.post(baseUrl, data, { crossdomain: true })
   return response.data
 }
 
