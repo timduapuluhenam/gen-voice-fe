771c0551451c9abbcd14a88c007b2350aa6111e5
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2021-12-22 14:43:04 +0700

    add auth route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import {
+  Routes,
+  Route
+} from 'react-router-dom'
 
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App () {
   return (
-    <div style={{ margin: '0', padding: '0' }}>
-      <Home/>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+    </Routes>
   )
 }
 
