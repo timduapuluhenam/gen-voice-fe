0d81b3561d9e5c921fbf21081d5695b8673575e8
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2021-12-23 20:21:17 +0700

    minor change

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,15 +5,17 @@ import Logo from '../assets/img/Logo.png'
 
 import style from '../assets/style/sidebar.module.css'
 
-import { MdDashboard, MdSettings, MdHelpOutline } from 'react-icons/md'
+import { MdDashboard, MdSettings, MdHelpOutline, MdLogout } from 'react-icons/md'
 import { HiDocumentReport, HiOutlineClock } from 'react-icons/hi'
 
 const Sidebar = () => {
   return (
     <div className='d-flex flex-column flex-shrink-0 p-0' style={{ width: '280px', height: '100vh', background: '#0D1A33' }}>
-      <Link to="/" style={{ textDecoration: 'none', background: '#0D1A33' }} className='p-3'>
-        <img src={Logo} width="56.25" height="50"/>
-        <span className={style.logo}>GenVoice</span>
+      <Link to="/" style={{ textDecoration: 'none', background: '#0D1A33', height: '12vh' }} className='d-flex justify-content-center'>
+        <div style={{ margin: 'auto' }}>
+          <img src={Logo} width="56.25" height="50" />
+          <span className={style.logo}>GenVoice</span>
+        </div>
       </Link>
       <div className='h-25 m-0 fs-4 text-white d-flex justify-content-center' style={{ background: '#1F3A57' }}>
         <div style={{ margin: 'auto' }}>Organization Name</div>
@@ -47,6 +49,12 @@ const Sidebar = () => {
           <a href='/#' className='nav-link text-white'>
           <MdHelpOutline className='align-middle'/>
           <span className='align-middle ms-1'>Help</span>
+          </a>
+        </li>
+        <li className='nav-item'>
+          <a href='/#' className='nav-link text-white'>
+          <MdLogout className='align-middle'/>
+          <span className='align-middle ms-1'>Logout</span>
           </a>
         </li>
       </ul>
