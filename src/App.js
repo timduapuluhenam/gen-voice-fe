8772f16b519c8769dbc1fca586c86b42a81e7e21
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2021-12-23 20:21:42 +0700

    minor change

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import {
 } from 'react-router-dom'
 
 import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
+// import Login from './pages/Login'
+// import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 
 function App () {
   return (
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/register" element={<Register/>}/>
+      {/* <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/> */}
 
       <Route path="/dashboard" element={<Dashboard/>}/>
     </Routes>
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,8 +4,13 @@ import Sidebar from '../components/Sidebar'
 
 const Dashboard = () => {
   return (
-    <div>
-      <Sidebar/>
+    <div className='d-flex justify-content-end'>
+      <Sidebar className='position-sticky' />
+      <div className='w-100'>
+        <nav className='bg-dark' style={{ height: '12vh' }}>
+
+        </nav>
+      </div>
     </div>
   )
 }
