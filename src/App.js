99d4733ddbde3a1456b1224dba45588753a5b096
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2021-12-23 21:13:06 +0700

    add logout route

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import {
 } from 'react-router-dom'
 
 import Home from './pages/Home'
-// import Login from './pages/Login'
-// import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Logout from './pages/Logout'
+import Register from './pages/Register'
 
 function App () {
   return (
     <Routes>
       <Route path="/" element={<Home/>}/>
-      {/* <Route path="/login" element={<Login/>}/>
-      <Route path="/register" element={<Register/>}/> */}
-
       <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/logout" element={<Logout/>}/>
     </Routes>
   )
 }
