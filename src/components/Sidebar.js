1cfebbd4c044e62766a1e4ed087e5f908e524101
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2021-12-23 16:43:42 +0700

    minor change

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,13 +3,53 @@ import { Link } from 'react-router-dom'
 
 import Logo from '../assets/img/Logo.png'
 
+import style from '../assets/style/sidebar.module.css'
+
+import { MdDashboard, MdSettings, MdHelpOutline } from 'react-icons/md'
+import { HiDocumentReport, HiOutlineClock } from 'react-icons/hi'
+
 const Sidebar = () => {
   return (
-    <div className='d-flex flex-column flex-shrink-0 p-3 bg-dark' style={{ width: '280px' }}>
-      <Link to="/">
+    <div className='d-flex flex-column flex-shrink-0 p-0' style={{ width: '280px', height: '100vh', background: '#0D1A33' }}>
+      <Link to="/" style={{ textDecoration: 'none', background: '#0D1A33' }} className='p-3'>
         <img src={Logo} width="56.25" height="50"/>
-        <span></span>
+        <span className={style.logo}>GenVoice</span>
       </Link>
+      <div className='h-25 m-0 fs-4 text-white d-flex justify-content-center' style={{ background: '#1F3A57' }}>
+        <div style={{ margin: 'auto' }}>Organization Name</div>
+      </div>
+      <ul className='nav nav-pills flex-column mb-auto' style={{ background: '#1F3A57' }}>
+        <li className={`nav-item ${style.active}`}>
+          <a href='/#' className='nav-link text-white'>
+          <MdDashboard className='align-middle'/>
+          <span className='align-middle ms-1'>Dashboard</span>
+          </a>
+        </li>
+        <li className='nav-item'>
+          <a href='/#' className='nav-link text-white'>
+          <HiDocumentReport className='align-middle'/>
+          <span className='align-middle ms-1'>Report</span>
+          </a>
+        </li>
+        <li className='nav-item'>
+          <a href='/#' className='nav-link text-white'>
+          <HiOutlineClock className='align-middle'/>
+          <span className='align-middle ms-1'>Tracks</span>
+          </a>
+        </li>
+        <li className='nav-item'>
+          <a href='/#' className='nav-link text-white'>
+          <MdSettings className='align-middle'/>
+          <span className='align-middle ms-1'>Settings</span>
+          </a>
+        </li>
+        <li className='nav-item'>
+          <a href='/#' className='nav-link text-white'>
+          <MdHelpOutline className='align-middle'/>
+          <span className='align-middle ms-1'>Help</span>
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
