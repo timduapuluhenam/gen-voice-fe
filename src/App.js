d17498c34430987eaa9d3136a18152fb598079a5
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2021-12-29 20:38:44 +0700

    delete logout component

--- src/App.js
+++ src/App.js
@@ -7,17 +7,15 @@ import {
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
-import Logout from './pages/Logout'
 import Register from './pages/Register'
 
 function App () {
   return (
     <Routes>
       <Route path="" element={<Home/>}/>
-      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/dashboard/*" element={<Dashboard/>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/register" element={<Register/>}/>
-      <Route path="/logout" element={<Logout/>}/>
     </Routes>
   )
 }
