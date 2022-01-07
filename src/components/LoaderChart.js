a574548e08e019a52ea99f4c1efc2025e0062322
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2022-01-07 13:18:55 +0700

    add loader chart

---
+++ src/components/LoaderChart.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ContentLoader from 'react-content-loader'
+
+const LoaderChart = (props) => (
+  <ContentLoader
+    speed={2}
+    width={1280}
+    height={720}
+    viewBox="0 0 1280 720"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+    {...props}
+  >
+    <rect x="5" y="243" rx="0" ry="0" width="83" height="477" />
+    <rect x="114" y="64" rx="0" ry="0" width="83" height="663" />
+    <rect x="246" y="202" rx="0" ry="0" width="83" height="537" />
+    <rect x="349" y="79" rx="0" ry="0" width="83" height="663" />
+    <rect x="465" y="91" rx="0" ry="0" width="83" height="663" />
+    <rect x="566" y="227" rx="0" ry="0" width="83" height="537" />
+  </ContentLoader>
+)
+
+export default LoaderChart
