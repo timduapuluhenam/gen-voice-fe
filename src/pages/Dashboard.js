e295da7e6ab265333f5d279b0663c289af2b3956
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2022-01-13 21:23:28 +0700

    add file validation

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -14,6 +14,8 @@ import readXlsxFile from 'read-excel-file'
 import Papa from 'papaparse'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import { useTitle } from 'react-use'
+import { useDispatch } from 'react-redux'
+import { setPage } from '../utils/reducers/pageReducer'
 
 ReactModal.setAppElement('#root')
 
@@ -24,6 +26,9 @@ const Dashboard = () => {
   const [extension, setExtension] = useState('')
   const [toggleUpload, setToggleUpload] = useState(false)
   const [date, setDate] = useState(new Date())
+
+  const dispatch = useDispatch()
+  dispatch(setPage('Dashboard'))
 
   const navigate = useNavigate()
 
@@ -53,6 +58,17 @@ const Dashboard = () => {
       readXlsxFile(selectedFile).then(rows => {
         rows.shift()
         for (const row of rows) {
+          const emailChecker = /\S+@\S+\.\S+/
+          if (!emailChecker.test(row[1])) {
+            alert('Your schema is not valid')
+            return null
+          }
+
+          const numberChecker = /^[0-9]*$/
+          if (!numberChecker.test(row[2])) {
+            alert('One of your amount row is not valid. Please fix it!')
+            return null
+          }
           const temp = {
             name: row[0],
             email: row[1],
@@ -70,6 +86,17 @@ const Dashboard = () => {
           tempData.shift()
           tempData.pop()
           for (const row of tempData) {
+            const emailChecker = /\S+@\S+\.\S+/
+            if (!emailChecker.test(row[1])) {
+              alert('One of your email row is not valid. Please fix it!')
+              return null
+            }
+
+            const numberChecker = /^[0-9]*$/
+            if (!numberChecker.test(row[2])) {
+              alert('One of your amount row is not valid. Please fix it!')
+              return null
+            }
             const temp = {
               name: row[0],
               email: row[1],
@@ -87,7 +114,7 @@ const Dashboard = () => {
   return (
     <div className='d-flex justify-content-end' style={{ overflowX: 'hidden' }}>
       <Sidebar/>
-      <div className={style.headerDashboard}>
+      <div className={style.container}>
         <nav className={`d-flex justify-content-between ${style.headerDashboard}`} style={{ height: '12vh' }}>
           <div style={{ margin: 'auto 20px' }} className={style.titleDashboard}>
             Dashboard
@@ -98,7 +125,7 @@ const Dashboard = () => {
             </button>
             <ReactModal isOpen={toggleUpload} contentLabel="Upload your data" className={style.fileUpload} onRequestClose={handleToggleUpload}>
               <form className='form-control p-4' onSubmit={handleUploadForm}>
-              <h3>Upload your data</h3>
+                <h3>Upload your data</h3>
                 <div className='my-3'>
                   <label>Invoice Name</label>
                   <input type='text' className='form-control' name='invoice_name' placeholder='Enter your invoice name' onChange={e => setInvoiceName(e.target.value)}/>
