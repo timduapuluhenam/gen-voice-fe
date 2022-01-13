be5d4a97cf4d8e0b606d9bdb9b4afd0ebab42837
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2022-01-13 21:29:01 +0700

    add a request

--- src/services/invoice.js
+++ src/services/invoice.js
@@ -10,3 +10,12 @@ export const addInvoice = async (data, token) => {
   })
   return response.data
 }
+
+export const getInvoice = async token => {
+  const response = await axios.get('https://gen-voice-be.herokuapp.com/invoices/generate_invoice', {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+  return response.data
+}
