19bc746223e1b00733016b908425a5354e8bbfc4
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2022-01-13 17:10:52 +0700

    add report page

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Reports from './pages/Reports'
 
 function App () {
   return (
     <Routes>
       <Route path="" element={<Home/>}/>
       <Route path="/dashboard/*" element={<Dashboard/>}/>
+      <Route path="/reports/*" element={<Reports />}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/register" element={<Register/>}/>
     </Routes>
