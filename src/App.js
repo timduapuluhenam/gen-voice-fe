a66fd179bd15aec00f62d732630235e643842f74
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2022-01-21 16:39:58 +0700

    add new routes

--- src/App.js
+++ src/App.js
@@ -9,15 +9,23 @@ import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Reports from './pages/Reports'
+import Help from './pages/Help'
+import Error from './pages/Error'
+import Feature from './pages/Feature'
+import Settings from './pages/Settings'
 
 function App () {
   return (
     <Routes>
-      <Route path="" element={<Home/>}/>
+      <Route path="/" element={<Home/>}/>
       <Route path="/dashboard/*" element={<Dashboard/>}/>
       <Route path="/reports/*" element={<Reports />}/>
+      <Route path="/settings" element={<Settings/>}/>
+      <Route path="/features" element={<Feature/>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/register" element={<Register/>}/>
+      <Route path="/help" element={<Help/>}/>
+      <Route path="*" element={<Error/>}/>
     </Routes>
   )
 }
